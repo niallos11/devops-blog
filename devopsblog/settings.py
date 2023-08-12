f7d9a0252b556fd0268248095815ce83bd02e2c9
niallos11 <>
2023-08-12 01:00:16 +0000

    Add allow host for heroku app

--- devopsblog/settings.py
+++ devopsblog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-2#qz$^0cwa2qd%42^ebg)x(pu8lhu5oeby0s6xfe_)6y7dq0_t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-niallos11-devops-blog-sr1cmktcz5.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-niallos11-devops-blog-sr1cmktcz5.us2.codeanyapp.com','devopsblog-9ee0000374f1.herokuapp.com']
 
 
 # Application definition
