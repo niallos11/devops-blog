49997e072eb07e4099458e91bf69251ea8ea0ecc
niallos11 <>
2023-08-12 00:58:40 +0000

    Change the templates DIRS

--- devopsblog/settings.py
+++ devopsblog/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'devopsblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
